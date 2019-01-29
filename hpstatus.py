930a755126b33f1bf0b0fa64170bab4acf7351cd
Jeffrey Steward <jeffrey.steward@enbridge.com>
2019-01-29 16:02:49 -0700

    Added the line formatter

--- hpstatus.py
+++ hpstatus.py
@@ -9,7 +9,8 @@ parser.add_argument("feature", choices=FEATURES, help="The feature to display th
 parser.add_argument("-f", "--format", default="line", choices=FORMATS, help="The format to display the status in")
 parser.add_argument("--header", action="store_true", help="Adds a header to the CSV format, defaults to no header")
 parser.add_argument("--pretty", action="store_true", help="Pretty prints the JSON format, defaults to off")
+parser.add_argument("--prefix", default="hp_", help="Prefix to prepend to the measurement name in the line format")
 
 args = parser.parse_args()
 
-status = get_report(args.feature, args.format, {"header": args.header})
+status = get_report(args.feature, args.format, {"header": args.header, "pretty": args.pretty, "prefix": args.prefix})
--- hpstatus/report.py
+++ hpstatus/report.py
@@ -1,54 +1,76 @@
 import io
 import csv
+import json
 import hpstatus.status as status
 
+# The formats that are supported
 FORMATS = [
-	"line",
-	"csv",
-	"json"
+    "line",
+    "csv",
+    "json"
 ]
 
 # These keys are used as tags for each feature
+TAGS = {
+    "fans": ["id", "location"],
+    "powermeter": ["id"],
+	"powersupply": ["id"],
+	"temp": ["id", "location"]
+}
 
 def get_report(feature, format, opts):
-	data = []
-	if feature == "fans":
-		data = status.get_fans()
-	elif feature == "powermeter":
-		data = status.get_powermeter()
-	elif feature == "powersupply":
-		data = status.get_powersupply()
-	elif feature == "temp":
-		data = status.get_temp()
-	else:
-		raise ValueError("Invalid feature passed")
-	
-	value = ""
-	if format == "line":
-		value = _to_line(data)
-	elif format == "csv":
-		value = _to_csv(data, opts["header"])
-	elif format == "json":
+    data = []
+    if feature == "fans":
+        data = status.get_fans()
+    elif feature == "powermeter":
+        data = status.get_powermeter()
+    elif feature == "powersupply":
+        data = status.get_powersupply()
+    elif feature == "temp":
+        data = status.get_temp()
+    else:
+        raise ValueError("Invalid feature passed")
+    
+    value = ""
+    if format == "line":
+        value = _to_line(data, feature, opts["prefix"])
+    elif format == "csv":
+        value = _to_csv(data, opts["header"])
+    elif format == "json":
         value = _to_json(data, opts["pretty"])
-	else:
-		raise ValueError("Invalid format passed")
+    else:
+        raise ValueError("Invalid format passed")
 
-	print(value)
+    print(value)
 
-def _to_line(data):
-	return ""
+def _to_line(data, feature, prefix=""):
+    tags = TAGS[feature]
+    lines = []
+    for row in data:
+        # The measurement name is the first tag
+        row_tags = [prefix + feature]
+        for tag in tags:
+            row_tags.append("{}={}".format(tag, row[tag]))
+
+        row_values = []
+        for key, value in row.items():
+            if key not in tags:
+                row_values.append("{}={}".format(key, value))
+
+        lines.append("{} {}".format(",".join(row_tags), ",".join(row_values)))
+    return "\n".join(lines)
 
 def _to_csv(data, header=True, delimiter=","):
-	keys = list(data[0])
+    keys = list(data[0])
 
-	output = io.StringIO()
+    output = io.StringIO()
     writer = csv.DictWriter(output, fieldnames=keys, delimiter=delimiter)
 
-	if header:
-		writer.writeheader()
-	
-	for row in data:
-		writer.writerow(row)
+    if header:
+        writer.writeheader()
+    
+    for row in data:
+        writer.writerow(row)
     
     return output.getvalue()
 
