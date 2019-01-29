6d90cbeb5473fbdca81b97617c72bb4fc1149b45
Jeffrey Steward <jeffrey.steward@enbridge.com>
2019-01-29 16:07:15 -0700

    Changed the line formatter to use list comprehensions

--- hpstatus/report.py
+++ hpstatus/report.py
@@ -52,10 +52,7 @@ def _to_line(data, feature, prefix=""):
         for tag in tags:
             row_tags.append("{}={}".format(tag, row[tag]))
 
-        row_values = []
-        for key, value in row.items():
-            if key not in tags:
-                row_values.append("{}={}".format(key, value))
+        row_values = ["{}={}".format(key, value) for key, value in row.items() if key not in tags]
 
         lines.append("{} {}".format(",".join(row_tags), ",".join(row_values)))
     return "\n".join(lines)
