fa47d5a2312ddff4fc2cf8a37977f2348258da10
Jeffrey Steward <nulifier@gmail.com>
2019-01-29 23:23:13 -0700

    Fixed the quoting and integers of the line protocol

--- hpstatus/format.py
+++ hpstatus/format.py
@@ -9,18 +9,27 @@ FORMATS = [
     "json"
 ]
 
+def _format_line(value, tag=False):
+    if not tag and isinstance(value, str):
+        # Suround field values with double quotes
+        return '"{}"'.format(value)
+    elif isinstance(value, int) and not isinstance(value, bool):
+        return "{}i".format(value)
+    else:
+        return value
+
 def to_line(data, feature, tags=[], prefix=""):
     lines = []
     for row in data:
         # The measurement name is the first tag
         row_tags = [prefix + feature]
         for tag in tags:
-            row_tags.append("{}={}".format(tag, row[tag]))
+            row_tags.append("{}={}".format(tag, _format_line(row[tag], True)))
 
         row_values = []
         for key, value in row.items():
             if key not in tags:
-                row_values.append("{}={}".format(key, value))
+                row_values.append("{}={}".format(key, _format_line(value)))
 
         lines.append("{} {}".format(",".join(row_tags), ",".join(row_values)))
     return "\n".join(lines)
